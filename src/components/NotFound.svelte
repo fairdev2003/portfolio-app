40a0024bfafb544d4455e43520258af27adc1c13
Jakub Klimkiewicz <jakubklimkiewicz70@gmail.com>
2025-03-12 02:24:51 +0100

    Added custom 404 page

---
+++ src/components/NotFound.svelte
@@ -0,0 +1,16 @@
+<div class="flex justify-center flex-col items-center bg-red-500 w-full h-full p-5">
+	<img alt="error" src="https://img.icons8.com/?size=100&id=8122&format=png&color=FFFFFF"/>
+	<h1>
+		Oops. Błąd!
+	</h1>
+	<p class="text-lg flex gap-1">Strona pod podanym adresem nie istnieje!</p>
+	<a href="/" class="font-bold mt-3">Zabierz mnie stąd!</a>
+</div>
+
+<style>
+	a {
+			text-decoration: none;
+			color: inherit;
+			text-weight: bold;
+	}
+</style>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import NotFound from '../components/NotFound.svelte';
+</script>
+
+<div class="lg:grid mx-auto lg:grid-cols-4 md:flex md:flex-col md:justify-center w-full h-2/3">
+
+	{#if page.status === 404}
+		<div class="flex justify-center col-span-3 ">
+			<NotFound/>
+		</div>
+
+	{/if}
+</div>
