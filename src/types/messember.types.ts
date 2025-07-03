f35802ff3b44d9c960846319579775574a43cead
Klimson <kubaklimkiewicz1@gmail.com>
2025-07-03 12:23:29 +0200

    Added documentation for the code

--- src/types/messember.types.ts
+++ src/types/messember.types.ts
@@ -18,19 +18,22 @@ enum TextMessageReplyStatus {
 }
 
 type MessageWithReplyFields = {
-	replied?: boolean;
+	// if message is replied it will handle replied reference id also so its required to function
+	replied: boolean;
+	// it will tell ChatBox component which message it will referenced to
 	repliedReferenceId?: number;
 };
 
 enum MessageUserSide {
-	SENDER = 'sender', // nadawca (lewa strona)
-	RECIPIENT = 'recipient' // odbiorca (prawa strona)
+	SENDER = 'sender', // left side
+	RECIPIENT = 'recipient' // right side
 }
 
 type TextMessage<T extends TextMessageReplyStatus = TextMessageReplyStatus.NORMAL> =
 	BaseMessemberMessage & {
 		messageType: MessageEnums.MESSAGE;
 		messageContent: string;
+		// TODO: i guess i can type this to ImageMessage type
 		attachments?: string[];
 	} & (T extends TextMessageReplyStatus.REPLIED ? MessageWithReplyFields : {});
 
@@ -44,6 +47,7 @@ type ImagesLinks = string[];
 
 type ImageMessage<T extends TextMessageReplyStatus = TextMessageReplyStatus.NORMAL> =
 	BaseMessemberMessage & {
+		// message type will tell main component what component will be used for messages
 		messageType: MessageEnums.IMAGE;
 		// if there is one image message 
 		// will contain ImageMessage Component, 
@@ -56,11 +60,14 @@ enum ConversationPermissionLevel {
 	PROTECTED = 'protected'
 }
 
+// protected fields will handle 
+// all messages that will comes from backend instead of local files
 type ProtectedFields = {
 	protectedConversation: true;
 	backendId: string;
 };
 
+// what fields will local messages have
 type OpenedFields = {
 	messages: MessemberMessage[];
 };
@@ -74,9 +81,10 @@ type MessageBoxData<T extends ConversationPermissionLevel = ConversationPermissi
 	userBlocked: boolean;
 } & (T extends ConversationPermissionLevel.PROTECTED ? ProtectedFields : OpenedFields);
 
+// global type for all 
 type MessemberMessage = TextMessage | CallMessage | ImageMessage;
 
-// testy :D
+// type testing
 const openedConversation: MessageBoxData<ConversationPermissionLevel.OPENED> = {
 	id: '123',
 	linkUrl: '/chat/123',
