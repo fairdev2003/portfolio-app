a2e61f9e2110dddd6244eeea4cbb53e408cc13b6
Klimson <kubaklimkiewicz1@gmail.com>
2025-07-03 21:42:20 +0200

    Added /lore

---
+++ src/routes/lore/+page.svelte
@@ -0,0 +1,26 @@
+<div class="col-span-2 flex flex-col gap-5">
+    <h2>Rozdanie i fajerwerki</h2>
+
+    <p>
+        Jest to dla mnie bardzo wazna strona na etapie mojego zycia. Nie dosc ze moge wyrazic siebie na jakims etapie tym KIM jestem to jeszcze chcialbym pokazac wszytskim ze swiat nie jest czarno bialy i zawsze 
+        mozna liczyc na kogos pomoc nawet 
+        jezeli sie tego nigdy po tej osobie nie spoedziewales.
+    </p>
+
+    <p> 
+        Chcialbym zaczac od tego ze przez bardzo dlugi okres czasu czulem sie naprawde zle zarowno ze samym soba jak z innymi. Duzo tez na tym etapie ludzie skrzywdzilem. 
+        Nie myslalem nidgy ze to w jaki sposob mysle nie bylo absolutnie zdrowe
+    </p>
+
+    <p>
+       Nie jest to zaden osad w moja strone czy innych.
+       Po prostu chcialbym zakonczyc ten rozdzial w moim zyciu i wynies z tego lekcje
+    </p>
+
+    <p>
+        Jezeli czujesz sie w tej chwili sam - to tak nie jest. Tez tak myslalem a znikad pojawil sie ktos kto powiedzial ze "bedzie dobrze" i wlasnie jest dobrze.
+    </p>
+
+    <p>Pamietaj duszyczko nie jestes sam!</p>
+    
+</div>
