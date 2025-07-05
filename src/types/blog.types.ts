60279f160336b908b5f203167f6a6e5d16368e46
fairdev2003 <jakubklimkiewicz70@gmail.com>
2025-07-05 18:55:28 +0200

    Added some blog post typing and refactored /blog/+layout.ts

---
+++ src/types/blog.types.ts
@@ -0,0 +1,22 @@
+export type BlogWindowProps = {
+	navigation?: Partial<BlogNavigator>;
+	references?: References[];
+	routeName: string;
+	route: string;
+};
+
+export type BlogNavigator = {
+	left: NavigatorButtonProps;
+	right: NavigatorButtonProps;
+};
+
+export type NavigatorButtonProps = {
+	value: string;
+	desc?: string;
+	className?: string;
+};
+
+export type References = {
+	name: string;
+	route: string;
+};
