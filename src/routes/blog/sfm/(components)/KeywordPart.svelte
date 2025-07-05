cdcab58fa4363bbc33822827afafb644572f8286
Klimson <kubaklimkiewicz1@gmail.com>
2025-07-05 06:00:43 +0200

    Fixed KeywordPart hovering problem

--- src/routes/blog/sfm/(components)/KeywordPart.svelte
+++ src/routes/blog/sfm/(components)/KeywordPart.svelte
@@ -10,6 +10,5 @@
 
 <span class={`group relative cursor-help ${className}`}>
 	<!-- Tooltip -->
-	<div class="absolute bottom-20 z-99 hidden size-20 bg-white group-hover:flex"></div>
 	<slot />
 </span>
