d134c94fa249e04b8c3155d932dafe72d8146dc3
Klimson <kubaklimkiewicz1@gmail.com>
2025-07-05 06:33:00 +0200

    SectorButton added

---
+++ src/routes/blog/(components)/SectorButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		children: Snippet;
+		href?: string;
+	};
+
+	const { children, href = '' }: Props = $props();
+</script>
+
+<button
+	onclick={() => {
+		goto(href);
+	}}
+	class="cursor-pointer rounded-lg border-1 border-gray-950 bg-gray-700 p-2 px-6 text-sm hover:bg-gray-800"
+>
+	{@render children()}
+	{'>'}
+</button>
