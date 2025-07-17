0fa35fa7da4a7653fdabef2baf42799d6a7aa072
Klimson <kubaklimkiewicz1@gmail.com>
2025-07-18 00:57:40 +0200

    First part of lore section

---
+++ src/routes/blog/fireworks/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import SideBlogBar from '../(components)/SideBlogBar.svelte';
+	import ResponsiveContainer from '../../../components/responsive/ResponsiveContainer.svelte';
+	import ResponsiveWrapper from '../../../components/ResponsiveWrapper.svelte';
+
+	let { children, data } = $props();
+</script>
+
+<ResponsiveWrapper>
+	{@render children()}
+	<ResponsiveContainer cols={1} className="p-5 col-span-1 flex flex-col gap-2">
+		<SideBlogBar route={data.route} />
+	</ResponsiveContainer>
+</ResponsiveWrapper>
---
+++ src/routes/blog/fireworks/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import ResponsiveContainer from '../../../components/responsive/ResponsiveContainer.svelte';
+</script>
+
+<ResponsiveContainer cols={2} className="flex flex-col gap-y-3 col-span-2">
+	<h2>Rozdanie i fajerwerki</h2>
+</ResponsiveContainer>
