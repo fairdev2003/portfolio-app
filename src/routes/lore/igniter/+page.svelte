a4ed9df3c8bc207eecfe088f0b5d5c1535275642
fairdev2003 <jakubklimkiewicz70@gmail.com>
2025-07-19 00:53:59 +0200

    removed bad ascii chat gpt characters

--- src/routes/lore/igniter/+page.svelte
+++ src/routes/lore/igniter/+page.svelte
@@ -8,7 +8,7 @@
 
 	<p>
 		Po głębokiej analizie zachowań, które mną kierowały, mogę śmiało stwierdzić, że zapalnikiem
-		trwałego zaburzenia (prawdopodobnie osobowości – tutaj z wielkim znakiem zapytania, bo nie
+		trwałego zaburzenia (prawdopodobnie osobowości - tutaj z wielkim znakiem zapytania, bo nie
 		dostałem jeszcze żadnej oficjalnej diagnozy) był etap gimnazjum. Był to etap bardzo kluczowy.
 		Samo środowisko było dla mnie zbyt "szybkie" na pewnym etapie i możliwe, że przez to później
 		było mi ciężko z regulacją emocji w relacjach z ludźmi, którymi się otaczałem. Wiele takich
@@ -21,7 +21,7 @@
 		Generalnie sam start był dla mnie bardzo ciężki. Moja nadinterpretacja wszystkiego spowodowała,
 		że już od samego początku przebywania z ludźmi miałem konflikty, które mnie powoli niszczyły.
 		Byłem prześladowany w gimnazjum. Mimo tego emocjonalnego poniżania bardzo chciałem być częścią
-		grupy, co nie miało żadnego sensu – z perspektywy każdej normalnej osoby. Dla mojej głowy nie
+		grupy, co nie miało żadnego sensu - z perspektywy każdej normalnej osoby. Dla mojej głowy nie
 		było to takie oczywiste. Bardzo bałem się samotności. Była dla mnie gorsza niż emocjonalne
 		zranienia czy groźby pobicia. Mimo tej przynależności na siłę i tak się bałem. Często było tak,
 		że bałem się pójść do szkoły, bo ktoś dzień wcześniej powiedział, że mnie pobije. Dużą raną było
@@ -32,42 +32,42 @@
 		miejsca w ławce, i ona powiedziała coś w stylu "Weźcie go ode mnie". Takie sytuacje stale
 		potęgowały moje dalsze myślenie o sobie i poczucie własnej wartości. Uważałem, że jestem
 		"gorszym modelem" i że nie zasługuję na nic. Tak jak wspomniałem wcześniej, te mechanizmy
-		przetrwały do etapu wyprowadzki do Krakowa – co może być mechanizmem obronnym na to, co się
-		działo, ale jak już wspomniałem – nie mam jeszcze żadnej oficjalnej diagnozy, bo dopiero mam
+		przetrwały do etapu wyprowadzki do Krakowa - co może być mechanizmem obronnym na to, co się
+		działo, ale jak już wspomniałem - nie mam jeszcze żadnej oficjalnej diagnozy, bo dopiero mam
 		psychiatrę za 2 tygodnie.
 	</p>
 
 	<p>
 		Wszystkie sytuacje społeczne, w których byłem poniżany, były dla mnie ciężkie. Przez cały etap
 		szkoły byłem tak wykluczony z życia klasy, że nawet nie było mnie na głównej grupie
-		messengerowej klasy – przez większość czasu. Były sytuacje, gdzie koledzy robiąc sobie ze mnie
-		jaja, tworzyli grupy, gdzie wszystko skupiało się na mnie – po części było to dla mnie w
+		messengerowej klasy - przez większość czasu. Były sytuacje, gdzie koledzy robiąc sobie ze mnie
+		jaja, tworzyli grupy, gdzie wszystko skupiało się na mnie - po części było to dla mnie w
 		porządku z perspektywy czasu, ale z drugiej strony wiem, że w dalszym ciągu byłem wykluczany.
-		Dla wielu osób ten mechanizm może wydawać się dziwny – bo po co się ich trzymałem? Dla mnie w
+		Dla wielu osób ten mechanizm może wydawać się dziwny - bo po co się ich trzymałem? Dla mnie w
 		tym momencie najważniejsze było to, że nie mam poczucia samotności. Mimo wielu emocjonalnych
-		zranień – trzymałem się prześladowców, i mimo wszystko i tak byłem samotny :(
+		zranień - trzymałem się prześladowców, i mimo wszystko i tak byłem samotny :(
 	</p>
 
 	<p>
 		Głównym też czynnikiem, dlaczego mogło być aż tak źle, jest fakt, że rodzice nie przekazali mi
-		żadnej emocjonalnej pomocy podczas tego okresu. Czułem się bardzo samotny – zarówno ze strony
+		żadnej emocjonalnej pomocy podczas tego okresu. Czułem się bardzo samotny - zarówno ze strony
 		grupy, jak i rodziców. Rodzice pomagali mi bardziej "materialnie", czyli dzwonili na policję czy
 		kłócili się z rodzicami osób, które mnie prześladowały. Natomiast ani razu nie miałem wsparcia
 		typu "Będzie dobrze", "Chodź, przytulę cię". Czułem ogromną suchość w tym wszystkim, wręcz
 		obojętność. Często było tak, że obwiniali mnie za to, że jestem taki "wybuchowy" i że nie umiem
-		się dogadać z grupą. Nie czułem na żadnym etapie wsparcia – wręcz pogarszali mój stan.
+		się dogadać z grupą. Nie czułem na żadnym etapie wsparcia - wręcz pogarszali mój stan.
 	</p>
 
 	<p>
-		Pamiętam do dziś, jak załatwili mi psychologa w szkole – a w zasadzie sądzę, że bardziej
+		Pamiętam do dziś, jak załatwili mi psychologa w szkole - a w zasadzie sądzę, że bardziej
 		wychowawca, który pogorszył mój stan psychiczny jeszcze bardziej. Nie jestem do końca pewny
 		dlaczego tam chodziłem. Psycholog działał na zasadzie "odgrywania scenek ze Star Warsów".
 		Możliwe, że miało to wydźwięk taki, że "nie boję się powiedzieć, czym się interesuję" albo jakaś
-		forma terapii behawioralnej – nie znam się, nie analizowałem tego, w jaki sposób miał działać
+		forma terapii behawioralnej - nie znam się, nie analizowałem tego, w jaki sposób miał działać
 		ten psycholog. Natomiast wiem, że czułem poniżenie, gdy przyszedł do klasy i mnie wywołał. Klasa
-		wiedziała, że to psycholog, więc zaczęli się śmiać ze mnie – co raczej, jak pewnie się
+		wiedziała, że to psycholog, więc zaczęli się śmiać ze mnie - co raczej, jak pewnie się
 		domyślacie, nie pomogło w moim stanie. Już chodząc do tego psychologa (mimo że fizycznie było to
-		bardzo po zajęciach, bo kończyłem często o 15, a psycholog był po 16–17), miałem myślenie typu –
+		bardzo po zajęciach, bo kończyłem często o 15, a psycholog był po 16-17), miałem myślenie typu -
 		jestem gorszy od nich, bo chodzę do psychologa i odgrywam jakieś "durne scenki z filmów".
 	</p>
 
@@ -76,14 +76,14 @@
 		relacji, ale byłem w tak niestabilnym środowisku, że taka relacja była wręcz niemożliwa. Przez
 		prawdopodobne ADHD miałem też problemy z zadbaniem o siebie i swój wygląd. Przez to byłem
 		jeszcze bardziej wyszydzany. Marzenia były bardzo intensywne. Często wręcz utożsamiałem się z
-		bohaterem filmu – i głęboko marzyłem, że może kiedyś mnie to spotka, mimo skrajnego lęku przed
+		bohaterem filmu - i głęboko marzyłem, że może kiedyś mnie to spotka, mimo skrajnego lęku przed
 		ludźmi.
 	</p>
 
 	<p>
 		Było wiele sytuacji, w których doświadczałem upokorzenia i jeszcze więcej wątków, które mógłbym
 		poruszyć, ale uważam, że te, które opisałem, w pełni oddają to, co wtedy czułem. Nie muszę chyba
-		dodawać, że moja psycha prawdopodobnie próbowała mnie chronić przed tym wszystkim – bo przez
+		dodawać, że moja psycha prawdopodobnie próbowała mnie chronić przed tym wszystkim - bo przez
 		długi czas nie postrzegałem tego jako traumy, lecz raczej jako "jakiś rozdział życia". Można
 		nawet powiedzieć, że częściowo o tym zapomniałem
 	</p>
