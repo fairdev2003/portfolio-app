cf7de91d0fb0ced64e5c0513216dfe1b53a8a649
fairdev2003 <jakubklimkiewicz70@gmail.com>
2025-07-19 08:37:19 +0200

    Added s

---
+++ src/routes/lore/stereotypes/+page.svelte
@@ -0,0 +1,105 @@
+<script>
+	import ResponsiveContainer from '../../../components/responsive/ResponsiveContainer.svelte';
+	import PageNavigator from '../../blog/(components)/PageNavigator.svelte';
+</script>
+
+<ResponsiveContainer cols="2" className="flex flex-col gap-y-5 col-span-2">
+	<h2>Stereotypy</h2>
+
+	<p>
+		Zaznacza na wstepie ze to bedzie ostatna "strona" tej mojej przygody. Nie myslalem ze to kiedys
+		powiem ale chyba wiem z czym pojde do psychiatry. Nie tylko z ADHD ale z czyms co jest o wiele
+		powazniejszym stanem, ktore mna kierowaly
+	</p>
+
+	<p>
+		Przedewszytskim chce zazanaczyc ze nie chce siebie diagnozowac ale stworzyc sobie bezpieczna
+		przestrzen do zrozumienia siebie i swoich mechanizmow ktore mna kierowaly przez caly czas. Ten
+		tekst zostanie wykorzystany dla psychiatry ktorego mam 28 lipca. Na tym etapie jestem w stanie
+		wydac wszystkie oszczednosci aby wiedziec co sie ze mna dzialo - to nie impuls, to chec lepszego
+		jutra.
+	</p>
+
+	<p>Dobra przejdziemy do konkretow</p>
+
+	<p>
+		Przez caly okres uwazalem zaburzenie osobowisci Borderline jako cos bardzo cos zlozonego i
+		scisle zwiazanego z pewnoscia siebie. Mimo ze kruczkowo trzymalem sie schamtu "jak nie bede
+		dopszczal sie ryzykownych zachowan jak przelotne uprawianie seksu na miescie, naduzywanie
+		alkoholu czy ciecie sie" to nie moge miec tego zaburzenia. Najzabawniejsze jest to ze na pewnym
+		etapie jak dowiedzialem jak wyglada klasyczny border - wrecz mozna powiedziec ze marzylem zeby
+		go miec co tez bylo mechanizmem obronnym - marzylem o tym zeby podejmowac takie same deycyzje
+		jak osoby z klasynym borderem jak przelotne znajomosci (co wyjasnia pewne mechnizmy na poczatku
+		gimnazjum ktore sa do dzis) .Trzymalem sie schematu "jak nie jestem zbyt pewny siebie to nie
+		moge tego miec". Przez glebokie analizowanie schematow z gimnajzum do teraz moge smialo
+		stwierdzic - ze cos jest na rzeczy w kwestii mechanizmow bordera. Ale krotko dlaczego uwazam ze
+		moze byc to border.
+	</p>
+
+	<p>
+		Nadmienie ze gleboki paradoks "czy mam bordera" byl rozwazany gdy zapytalem sie ChataGPT czy
+		border moze wygladac jak "sinusoida". Nie utwierdzalem sie w przekonaniu ze border jest bardzo
+		zalezny od srodowiska. Nie myslalem ze przebywanie w srodowisku da mi poczucie zmiennosci
+		personalnej. Z jednej strony tutaj czujesz sie pewny siebie a tutaj - odcinasz sie. Nie zdawalem
+		sobie sprawy analizujac objawy klasycznego bordera - ze poczucie zmiennej "innosci" nie wynika z
+		paranoi tylko z ludzi z ktorymi przebywam. To ze teraz zaczynam inaczej siebie nawet w glupim
+		lustrze mowi ze cos bylo na rzeczy. Nie chodzi nawet o sam wyglad a o postrzeganie swojej osoby
+		w konkretnych sytguacjach spolecznych albo dlugo po nich. I to ze nie czulem zmiennosci
+		osobowosci - wcale nie oznacza ze tej zmiennosci nie mialem - co jest przerazajace. Co napewno
+		wiem ze bylo to gleboki wstyd przed introwertyzmem. Dla mnie w wielu sytuacjach introwertyzm byl
+		jak wewnetrzny wrog, ktorego chce za wszelka cene sie pozbyc. W wielu sytuacjach spolecznych
+		"wrecz" udawalem bycie ekstrwertykiem mimo ze nie mialem zasobow emocjonalnych aby czuc sie jak
+		ekstrewertyk. Ale rowniez czesto czulem gleboki wstyd ze tego ekstrewertyka udaje. To byl tak
+		skrajny konflikt, ktorego nie jestem nawet w stanie opisac slowami. Ale rowniez w tej kwestii ta
+		walka tych dwoch sil nie dawala mi stabilnego poczucia "JA" - dlatego w niektorych sytuacjach
+		wydawalem sie na pewnego siebie ale tez w niektorych sytuacjach czulem sie wycofany
+	</p>
+
+	<p>
+		- i to wycofanie bylo dla mnie najwiekszym wstydem - co moze tlumaczyc trzymania sie toksycznej
+		relacji z kolezanka ktora mna manipolowala. Chcialem zeby mnie widziala jako "ekstrewertyka" a
+		nie "wycofanego introwertyka". Co bolalo i nie dawalo mi zadnym etapie relacji poczucia
+		jakiejkolwiek stabilnosci i bezpieczenstwa. Kluczowa kwestia stal sie fakt ze chcialem sie wrecz
+		na niej "zemscic" gdy w pore zoorientowalem sie co ze mna robila. Natomiast mam teraz wrazenie
+		ze i tak dzialalem wtedy w kompletnej "niezgodzie" ze soba bo odejscie od kolezanki oznaczalo ze
+		bede samotny bo byla jedyna osoba ktora mialem na tym etapie, z ktora wychodzilem. A to ze
+		chcialem "cos jej zrobic" - bylo moim sposobem na radzenie sobie i zludnym poczuciem kontroli
+		ktorej nie mialem na zadnym etapie. Prawdopodobna impulsywnosc spowodowana ADHD mogla spowodowac
+		ze znowu dzialalem na "autopilocie" przez co znowu wychodzilem do ludzi z ktorymi sie nie
+		rozumialem. Oczywiscie skrajna potrzeba bliskosci znowu zadzialala - piszac ZNOWU do losowej
+		osoby ktora mnie nie znala aby poczuc to co kazdy na pewnym etapie zycia - milosc. I szczerze do
+		jakiegos momentu nie myslalem ze ta relacja z kolezanka miala wplyw - ale miala. Dalej
+		ksztaltowla mnie jako "nie pasujacego do tego swiata"
+	</p>
+
+	<i class="bg-gray-700 p-2 opacity-100"
+		>Doslownie moj glos wewnetrzny do nie rozmawial tak: "Albo czegos nie zrobisz teraz albo do
+		konca zycia bedziesz samotny"</i
+	>
+
+	<p>Dlaczego podkreslilem "klasycznego bordera"?</p>
+
+	<p>
+		Przez wiekszosc czasu utwierdzalem sie w przekonaniu ze napewno nie jest to border. Czesto nawet
+		wkraczalo to na tematy podejrzen ADHD, gdzie podobne hustawki nastrojow moga wystepowac. Ale
+		przy analizie wszystkich zachowan tez dopuszczalem mysl - hmm moze jest to AVPD (Unikowe
+		Zaburzenie Osobowosci). Ale konkretnie teraz uwazam ze praktycznie wiekszosc zachowan - a raczej
+		mechanizmow wzkazuje na Cichego Borderline. Nie chce sam sie diagnozowac gdyz nie jestem
+		specialista ale smialo moge stwierdzic ze wiekszosc objawow sie pokrywa - gdzie przy AVPD duzo
+		rzeczy ze soba kolidowalo - co jest kluczowe. To ze teraz czuje sie lepiej i nie boje sie ludzi
+		nie jest przypadkiem. Te "mini zawaly" o ktorych opowiadalem nie byly "lekiem spolecznym" a
+		zlozonym mechanizmem obronnym, ktory moze wystapic w przypadku borderline. Czuje ze to dalej we
+		mnie jest - dlatego ten tekst odczytam psychiatrze.
+	</p>
+
+	<PageNavigator
+		left={{ href: '/lore/igniter', value: 'Zapalnik' }}
+		right={{ href: '/lore/covid', value: 'Covid' }}
+	/>
+</ResponsiveContainer>
+
+<style>
+	.text {
+		opacity: 85%;
+	}
+</style>
