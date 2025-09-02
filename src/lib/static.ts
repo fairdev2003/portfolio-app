7dc16b39434cd7065cba9f12a74b47ab4319bece
fairdev2003 <jakubklimkiewicz70@gmail.com>
2025-09-02 15:06:47 +0200

    commit

--- src/lib/static.ts
+++ src/lib/static.ts
@@ -60,11 +60,11 @@ const websiteStack: TechStackType[] = [
 ];
 
 const pageSections = [
-	{ name: 'Index', description: '', path: '/' }
-	// { name: 'O mnie', description: 'Trochę informacji o mnie', path: '/about-me' },
-	// { name: 'Blog', description: 'Rzeczy ktore mnie interesuja', path: '/blog' },
-	// { name: 'Cytaty', description: 'Moje życiowe cytaty', path: '/quotes' },
-	// { name: 'Kontakt', description: 'Dostępne metody kontaktu', path: '/contact' }
+	{ name: 'Index', description: '', path: '/' },
+	{ name: 'O mnie', description: 'Trochę informacji o mnie', path: '/about-me' },
+	{ name: 'Blog', description: 'Rzeczy ktore mnie interesuja', path: '/blog' },
+	{ name: 'Cytaty', description: 'Moje życiowe cytaty', path: '/quotes' },
+	{ name: 'Kontakt', description: 'Dostępne metody kontaktu', path: '/contact' }
 ];
 
 type ProjectsProps = {
