c7d5e91cb84be270aad319c5bfaeafc72316b550
fairdev2003 <jakubklimkiewicz70@gmail.com>
2025-10-03 00:58:09 +0200

    Added pause and play state for user

--- src/routes/music/(components)/PlayButton.svelte
+++ src/routes/music/(components)/PlayButton.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import MusicController from '../music.svelte';
-	import CircularProgressBar from './CircularProgressBar.svelte';
 	import type { PlayButtonState } from './types/music';
 
 	type Props = {
@@ -10,54 +9,46 @@
 	let state = $state<PlayButtonState>('not-playing');
 	const { song_link }: Props = $props();
 
-	let progress: number = 0.65; // 65% np.
-
-	const size = 100;
-	const strokeWidth = 4;
-	const radius = 48 - strokeWidth / 2;
-	const circumference = 2 * Math.PI * radius;
-	let dashoffset: number;
 	$effect(() => {
-		dashoffset = circumference * (1 - Math.max(0, Math.min(1, progress)));
+		if (MusicController.audio) {
+			MusicController.audio.onplay = () => (state = 'playing');
+			MusicController.audio.onpause = () => (state = 'not-playing');
+			MusicController.audio.onended = () => (state = 'not-playing');
+		}
 	});
-</script>
 
-<!-- svelte-ignore a11y_click_events_have_key_events -->
-<!-- svelte-ignore a11y_no_static_element_interactions -->
-
-<div
-	onclick={() => {
+	async function handleClick() {
 		if (!song_link) return;
 
 		if (!MusicController.audio) {
-			// either no audio loaded or new track selected
-
-			MusicController.GetMusic(song_link);
-			MusicController.PlayMusic();
+			await MusicController.GetMusic(song_link);
+			await MusicController.PlayMusic();
 		} else {
-			// same track, toggle play/pause
 			if (MusicController.audio.paused) {
-				MusicController.PlayMusic();
+				await MusicController.PlayMusic();
 			} else {
-				MusicController.PauseMusic();
+				await MusicController.PauseMusic();
 			}
 		}
-	}}
+	}
+</script>
+
+<div
+	onclick={handleClick}
 	class="flex size-[40px] cursor-pointer items-center justify-center rounded-full bg-neutral-700"
 >
 	<svg width="35" height="35" viewBox="0 0 100 100">
-		<!-- Tło niewidoczne -->
-		<circle cx="50" cy="50" r="48" fill="transparent" stroke="none" />
-
-		<!-- Okrąg progressu -->
-
-		<!-- Play icon -->
-		<polygon
-			points="35,25 35,75 75,50"
-			fill="white"
-			stroke="white"
-			stroke-width="1"
-			stroke-linejoin="round"
-		/>
+		{#if state === 'not-playing'}
+			<polygon
+				points="35,25 35,75 75,50"
+				fill="white"
+				stroke="white"
+				stroke-width="1"
+				stroke-linejoin="round"
+			/>
+		{:else}
+			<rect x="30" y="25" width="12" height="50" fill="white" />
+			<rect x="58" y="25" width="12" height="50" fill="white" />
+		{/if}
 	</svg>
 </div>
