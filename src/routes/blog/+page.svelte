5b3a32c61016c1accc7f63b853a47bada90ffaec
fairdev2003 <jakubklimkiewicz70@gmail.com>
2025-10-05 13:17:09 +0200

    Re-themed blod route

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -5,6 +5,8 @@
 	import SectorButton from './(components)/SectorButton.svelte';
 	import SFM from '../../assets/sfm.png';
 	import PagesNavigation from '../../components/typography/PagesNavigation.svelte';
+	import ProjectCard from '../about-me/sectors/components/ProjectCard.svelte';
+	import Heading from '../../components/typography/Heading.svelte';
 
 	const { data } = $props();
 </script>
@@ -16,33 +18,20 @@
 			{ name: 'BLOG', route: '/about-me' }
 		]}
 	/>
-	<h2>Sekcja blogowa</h2>
+	<Heading>Blog</Heading>
+	<div class="h-3"></div>
 
 	<!-- move this to component -->
 	<!-- svelte-ignore a11y_click_events_have_key_events -->
 	<!-- svelte-ignore a11y_no_static_element_interactions -->
 	<!-- svelte-ignore event_directive_deprecated -->
-	<div
-		on:click={() => {
+
+	<ProjectCard
+		name="Super Factory Manager"
+		description="Super Factory Manager to nowoczesny mod logistyczny do Minecrafta..."
+		logo={{ url: SFM, color: 'bg-gray-500' }}
+		onClick={() => {
 			goto('/blog/sfm');
 		}}
-		class="box flex cursor-pointer gap-5 rounded-xl p-5 hover:bg-[#131333]/50"
-	>
-		<div class="col-span-2 flex size-20 items-center justify-center">
-			<!-- svelte-ignore a11y_missing_attribute -->
-			<img src={SFM} class="" />
-		</div>
-		<div class="col-span-8">
-			<h3>SFM</h3>
-			<p class="">Super Factory Manager to nowoczesny mod logistyczny do Minecrafta...</p>
-		</div>
-	</div>
-	<div class="mt-2 flex justify-between">
-		<SectorButton left href={data.navigation?.left?.href}
-			>{data.navigation?.left?.value}</SectorButton
-		>
-		<SectorButton right href={data.navigation?.right?.href}
-			>{data.navigation?.right?.value}</SectorButton
-		>
-	</div>
+	/>
 </ResponsiveContainer>
