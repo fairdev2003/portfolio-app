7c17fd80529a4bcf7eaa6ab310c531b615366c42
fairdev2003 <jakubklimkiewicz70@gmail.com>
2025-10-05 06:02:21 +0200

    added project and modal navbar now is displaying project name instead of id

---
+++ src/routes/about-me/static/projects/HarcQuiz.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Heading from '../../../../components/typography/Heading.svelte';
+	import StatusBit from '../../sectors/components/StatusBit.svelte';
+	import type { Project, ProjectPage } from '../project.types';
+	import { projects } from '../projects';
+	import ProjectGeneralInfo from './components/ProjectGeneralInfo.svelte';
+
+	type Props = {
+		project?: Project;
+	};
+
+	const { project }: Props = $props();
+</script>
+
+{#if project}
+	<div class="lg:px-5">
+		<ProjectGeneralInfo {...project} />
+	</div>
+{/if}
