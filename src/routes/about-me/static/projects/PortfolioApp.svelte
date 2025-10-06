ffe62d8e664a36f641929958af82951771ca0c3f
fairdev2003 <jakubklimkiewicz70@gmail.com>
2025-10-06 20:27:07 +0200

    Added description to portfolio app project

--- src/routes/about-me/static/projects/PortfolioApp.svelte
+++ src/routes/about-me/static/projects/PortfolioApp.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Heading from '../../../../components/typography/Heading.svelte';
+	import Paragraph from '../../../../components/typography/Paragraph.svelte';
 	import StatusBit from '../../sectors/components/StatusBit.svelte';
 	import type { Project, ProjectPage } from '../project.types';
 	import { projects } from '../projects';
@@ -15,5 +16,15 @@
 {#if project}
 	<div class="lg:px-5">
 		<ProjectGeneralInfo {...project} />
+		<div class="mt-5 flex flex-col gap-3">
+			<Heading idTrackingDisabled>Opis</Heading>
+			<Paragraph
+				>Początkowo projekt był skierowany w celu stworzenia projektu na studia (za który tak swoją
+				drogą dostalem 5). Natomiast od jakiegoś czasu zaczynam bardzo lubieć dodaawać najróżniejsze
+				komponenty stworzone w 100% przeze mnie. Szczerze powiedziawszy nie lubie bibliotek z już
+				gotowo stworzonymi komponentami strony, bo po prostu lubie sój indywidualizm. Strona jest w
+				stylu</Paragraph
+			>
+		</div>
 	</div>
 {/if}
