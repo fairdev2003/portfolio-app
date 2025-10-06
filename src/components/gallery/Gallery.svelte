cc09381c0a909e92fa8f19f3eedca0e3fd1fd0ee
fairdev2003 <jakubklimkiewicz70@gmail.com>
2025-10-06 22:28:34 +0200

    Updated gallery look

--- src/components/gallery/Gallery.svelte
+++ src/components/gallery/Gallery.svelte
@@ -12,29 +12,44 @@
 	const scrollLeft = () => container.scrollBy({ left: -300, behavior: 'smooth' });
 	const scrollRight = () => container.scrollBy({ left: 300, behavior: 'smooth' });
 
-	const buttonClass =
-		'rounded-full size-18 bg-neutral-900/60 hover:bg-neutral-800/60 p-2 text-white cursor-pointer';
+	const scrollButonClass =
+		'absolute top-1/2 right-2 z-10 size-10 -translate-y-1/2 rounded bg-black/60 p-2 text-white';
+
+	type Props = {
+		items?: string[];
+	};
+
+	const { items = images }: Props = $props();
 </script>
 
-<div class="relative mx-auto flex max-w-4xl items-center gap-5">
+<div class="group relative mx-auto max-w-4xl">
 	<!-- strzałka w lewo -->
-	<button on:click={scrollLeft} class={buttonClass}>
-		{'<'}
+	<button
+		on:click={scrollLeft}
+		class="absolute top-1/2 left-2 z-10 size-10 -translate-y-1/2 cursor-pointer rounded bg-black/60 p-2 text-white"
+	>
+		‹
 	</button>
 
 	<!-- strzałka w prawo -->
+	<button
+		on:click={scrollRight}
+		class="absolute top-1/2 right-2 z-10 size-10 -translate-y-1/2 cursor-pointer rounded bg-black/60 p-2 text-white"
+	>
+		›
+	</button>
 
 	<!-- scrollable container -->
 	<div
 		bind:this={container}
 		class="scrollbar-thin scrollbar-thumb-gray-500 flex space-x-4 overflow-x-auto py-4"
 	>
-		{#each images as img}
-			<img src={img} alt="Gallery image" class="h-48 flex-shrink-0 rounded-lg object-cover" />
+		{#each items as img}
+			<img
+				src={img}
+				alt="Gallery image"
+				class="h-48 flex-shrink-0 rounded-lg border-1 border-neutral-800/60 object-cover"
+			/>
 		{/each}
 	</div>
-
-	<button on:click={scrollRight} class={buttonClass}>
-		{'>'}
-	</button>
 </div>
--- src/routes/about-me/static/projects/PortfolioApp.svelte
+++ src/routes/about-me/static/projects/PortfolioApp.svelte
@@ -38,7 +38,13 @@
 		<Heading padding>Galeria</Heading>
 		<div class="h-5"></div>
 
-		<Gallery />
+		<Gallery
+			items={[
+				'https://res.cloudinary.com/dzaslaxhw/image/upload/v1759781996/portfolio/9383b88d-8b38-4054-9216-8d32ade55701.png',
+				'https://res.cloudinary.com/dzaslaxhw/image/upload/v1759782259/portfolio/24403025-a9f0-43a1-8522-9f64f8a728ce.png',
+				'https://res.cloudinary.com/dzaslaxhw/image/upload/v1759782318/portfolio/e871a5fe-f58e-4c1c-99d9-8ad02a13769c.png'
+			]}
+		/>
 		<div class="h-20"></div>
 	</div>
 {/if}
