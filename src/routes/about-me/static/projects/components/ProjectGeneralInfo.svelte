34556fc0a9f1a09e590518348dee17d273cdd5ce
fairdev2003 <jakubklimkiewicz70@gmail.com>
2025-10-07 02:53:32 +0200

    Updated PortfolioApp page

--- src/routes/about-me/static/projects/components/ProjectGeneralInfo.svelte
+++ src/routes/about-me/static/projects/components/ProjectGeneralInfo.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Github } from '@lucide/svelte';
+	import { Calendar, Github } from '@lucide/svelte';
 	import Heading from '../../../../../components/typography/Heading.svelte';
 	import StatusBit from '../../../sectors/components/StatusBit.svelte';
 	import type { Project } from '../../project.types';
@@ -35,35 +35,42 @@
 </script>
 
 <div
-	class="flex-col gap-5 border-1 border-neutral-800/60 bg-neutral-900/60 p-5 lg:flex lg:flex-row lg:justify-start"
+	class="flex-col justify-center gap-5 border-1 border-neutral-800/60 bg-neutral-900/60 p-5 lg:flex lg:flex-row lg:justify-start"
 >
 	<div
-		class={`mx-auto flex size-40 items-center justify-center rounded-lg lg:mx-px ${project.logo?.color}`}
+		class={`mx-auto my-auto flex size-40 items-center justify-center rounded-lg lg:mx-px ${project.logo?.color}`}
 	>
 		<img alt="svelte" class="flex size-30 opacity-80" src={project.logo?.url} />
 	</div>
 	<div class="my-5">
+		<div class="mb-2 flex items-center gap-2 text-[gray]">
+			<Calendar color="gray" size="20px" />
+			<p class="text-[14px]">{project.startDate}</p>
+		</div>
 		<div class="flex items-center gap-2">
 			<Heading idTrackingDisabled class="flex items-center"><p>{project.name}</p></Heading>
-			<StatusBit status={project.status} />
 		</div>
 
-		<div class="mt-2 flex flex-col gap-2">
+		<div class="mt-4 flex flex-col gap-2">
 			{#each project.links as { type, url }}
 				{@render Link(url, type)}
 			{/each}
+		</div>
+		<div class="mt-3">
+			<StatusBit status={project.status} />
 		</div>
 	</div>
 </div>
 
 {#snippet Link(url: string, type: string)}
-	<span class="flex items-center gap-1">
+	<span class="flex items-center gap-2">
 		{#if type == 'repo'}
-			<Github color="white" size="20px" />
+			<Github color="gray" size="20px" />
 		{/if}
 		{#if type == 'demo'}
-			<EarthIcon color="white" size="20px" />
+			<EarthIcon color="gray" size="20px" />
 		{/if}
+
 		<a href={url} target="_blank">{url.replace('https://', '')}</a>
 	</span>
 
