61dc9d83a56a38998c20bce8d2c6c2cf7647b4ba
fairdev2003 <jakubklimkiewicz70@gmail.com>
2025-10-09 01:43:14 +0200

    Added flashing text

--- src/routes/cyberspace/+page.svelte
+++ src/routes/cyberspace/+page.svelte
@@ -3,10 +3,23 @@
 	import Maniek2 from './assets/graphics/retro_maniek86.gif';
 	import Maniek1 from './assets/graphics/maniek.png';
 	import RetroButton from './components/RetroButton.svelte';
+	import { onMount } from 'svelte';
+
+	let colored: boolean = $state(false);
+
+	onMount(() => {
+		setInterval(() => {
+			colored = !colored;
+		}, 500);
+	});
 </script>
 
 <div class="flex flex-col gap-5 p-5">
-	<p class="retro-p text-sm text-white">Cyber Space</p>
+	{#if colored}
+		<p class="retro-p text-4xl text-red-500">Cyber Space</p>
+	{:else}
+		<p class="retro-p text-4xl text-white">Cyber Space</p>
+	{/if}
 	<div class="flex gap-2">
 		{@render Graphic1()}
 		{@render Graphic2()}
