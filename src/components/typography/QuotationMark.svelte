cb8142a62f63fbcf4a0e4b6864fded5cf4fb679b
fairdev2003 <jakubklimkiewicz70@gmail.com>
2025-10-10 19:06:33 +0200

    Added QuotationMark component

---
+++ src/components/typography/QuotationMark.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+	import Paragraph from './Paragraph.svelte';
+
+	type Props = {
+		children: Snippet;
+		white?: boolean;
+	} & HTMLAttributes<HTMLParagraphElement>;
+
+	const { white = false, children, ...props }: Props = $props();
+</script>
+
+<div class="flex gap-3 text-white" {...props}>
+	<div class="w-1 self-stretch rounded-full bg-neutral-500" />
+	<Paragraph class="flex-1 leading-relaxed text-neutral-400">{@render children()}</Paragraph>
+</div>
