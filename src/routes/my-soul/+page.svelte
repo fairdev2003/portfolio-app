3c9850c7fffc68b15d53fa79ced6e3e5d1cc9d9f
fairdev2003 <jakubklimkiewicz70@gmail.com>
2026-04-03 00:54:28 +0200

    Added note for psychiastris

---
+++ src/routes/my-soul/+page.svelte
@@ -0,0 +1,171 @@
+<script lang="ts">
+	import ResponsiveContainer from '../../components/responsive/ResponsiveContainer.svelte';
+	import Heading from '../../components/typography/Heading.svelte';
+	import Paragraph from '../../components/typography/Paragraph.svelte';
+</script>
+
+<ResponsiveContainer className="col-span-1">
+	<div class="flex flex-col gap-8">
+		{@render OneStepForward()}
+		{@render Depression()}
+		{@render Stability()}
+		{@render Mania()}
+		{@render MidSchool()}
+		{@render Summary()}
+	</div>
+</ResponsiveContainer>
+
+{#snippet OneStepForward()}
+	<div class="flex flex-col gap-5">
+		<Heading id="start">Wstęp</Heading>
+		<Paragraph
+			>Na wstępie chciałbym podkreślić ze pisze to dlatego ze nie wiem ile czasu mi zostało - tego
+			dobrego mnie. Czuje ze nie panuje nad tym jak sie obudze w jakim nastroju a w tym złym nie
+			jestem opisac co sie ze mna dzieje. Chcialbym aby ta notka trafila do specialisty, poniewaz
+			przez to jak sie czuje teraz w tym momencie dobrze wiem na ile sprawa jest powazna i wiem ze
+			trzeba ta kwestie zalawtic. Chcialbym pozdrowic moich znajomych - mam wspaniala grupke
+			znajomych ktorzy naprawde pomagaja.</Paragraph
+		>
+	</div>
+{/snippet}
+
+{#snippet Depression()}
+	<div class="flex flex-col gap-5">
+		<Heading id="depression">Depresja</Heading>
+		<Paragraph
+			>Zaczne w pierwszej kolejnosci od rzeczy ktorej nie umiem opisac gdy jestem w tym stanie.
+			Chcialbym w pierwszej kolejnoci zaczac od tego ze doslownie czuje jakby mi sie cala osobowosc
+			zmienila. Jestem wtedy mega zamkniety, mimo ze ktos chce ze mna wyjsc ja po prostu nie mam
+			sily bo moja glowa gorzej dziala. Mam wtedy powazne problemy z koncetracja przez ktora niszczy
+			mi sie doslownie cale zycie. Nie potrafie skleic zadnego zdania sensownie do czlowieka co
+			bardziej napedza ta spirale. Przy okazji dopowiem ze czuje sie wtedy jakbym nie byl
+			czlowiekiem, jakbym stracil siebie na jakims etapie. Wtedy nic mnie nie cieszy, wstydze sie
+			swoich pasji, wstydze sie istnienia, ze zyje, ze oddycham, Mam bardzo powazne problemy wtedy z
+			higiena. Czuje sie odlaczony od swojego mozgu, czuje ze nie panuje nad tym co sie dzieje w
+			mojej glowie. Nie jestem w stanie opisac jak bardzo okropnie sie wtedy czuje. Jestem wtedy
+			wrazliwy na krytyke. Przez problemy z koncetracja stracilem juz kilka razy z praca bo po
+			prostu moj mozg wlacza wtedy jakies oszczeddzanie energii o ktore absolutnie nie prosilem. Nie
+			musze dodawac ze czuje ze kazdy mnie wtedy denerwuje, kazdego mam dosc mimo ze ja taki nie
+			jestem - kocham ludzi, lubie spedzac z nimi czas. Tak jak wspomnialem mam grupke znajomych ale
+			wtedy czuje ze nie pasuje do nich wogole. Przejmuje sie wszystkim co mnie otacza. Jest duzo
+			przykladow ktore moghe przytoczyc ale po prostu jest ich za duzo a chciałbym zmiescic sie w
+			jak najmniejszym metrazu tekstu
+		</Paragraph>
+	</div>
+{/snippet}
+
+{#snippet Stability()}
+	<div class="flex flex-col gap-5">
+		<Heading id="stability">Stabilność?</Heading>
+		<Paragraph
+			>Gdy depresja przechodzi nadchodzi etap ktory mozna opisac: "cisza przed burza". Tak jak
+			wczesniej wspominalem - nie wiem czy np nie obudze sie jutro w zlym nastroju. To jest taki
+			stan w ktorym wiesz ze jest dobrze ale masz z tylu glowy ze to nie jest stabilnosc ktorej
+			szukasz. Zaczne od tego ze czuje sie wtedy jak normalny czlowiek. Funkcjonuje wtedy normalnie
+			Pracuje, studiuje, nawet dbam o siebie. Mam wtedy w miare ulozone w glowie. W porownaniu do
+			depresyjnego stanu mam bardzo zywa glowe, wiele pomyslow - co jest kluczowe przy pracy ktora
+			wykonuje czyli budowanie stron internetowym, Jestem w stanie umyc zeby, zadbac o siebie, wyjsc
+			ze znajomymi bez stresu. Czuje ze moge oddetchnac, wiecej muzyki mi sie podoba, moj angielski
+			jest lepszy (przez lepsza koncetracje). Umiem normalnie rozmawiac, sklejac zdania. Umiem
+			zagadywac losowe osoby bez stresu, bez chaosu zalatwiac sprawy na miescie (notariusz etc).
+			Jestem w stanie sie rozwijac, poznawac nowe rzeczy. Umiem opisac komus co sie dzieje w danym
+			momencie ze mna jak jest troche gorzej. Ogolnie zupelna odwrotnosc depresyjnego stanu.
+			Niestety zauwazylem tendencje do zbyt wysokich górek nastroju w tym stanie ktore powoduja ze
+			zjazd do depresyjnego stanu jest o wiele mocniejszy. Dodam ze jak to pisze czuje wlasnie ten
+			stan stabilnosci :)</Paragraph
+		>
+	</div>
+{/snippet}
+
+{#snippet Mania()}
+	<div class="flex flex-col gap-5">
+		<Heading id="upup">Zbyt wysoka górka</Heading>
+		<Paragraph
+			>Ogólnie raz pojawilo sie tak bardzo wysoko. Wspomne ze oba stany - i wzgledna stabilnosc oraz
+			ta zbyt wysoka gorka objawia sie po duzym stresie. Wczesniej cos musialo wydarzyc co bardzo we
+			mnie uderzylo abym czul oba te stany. Zaczne od tego ze zanim poczulem ten zbyt podwyzszony
+			nastroj czulem sie tak jak w poprzednim akapicie - stabilnie. Opowiem od czego sie zaczelo.
+			Pamietam ze wtedy gralem z kolega. Czulem sie okropnie wtedy. Na status aktywnosci dalem sobie
+			jakas tam muzyke smieszna. Napisala do mnei stara kolezanka z pracy ktora wtedy juz tam nie
+			pracowala a ja tak. Oboje pytalismy sie co tam u ciebie i wogole. W pewnym momencie cos we
+			mnie wybuchlo - napisalem ze jest ciezko u mnie. Po tej wiadomosci kolezanka strasznie mocno
+			zareagowala. Myslala ze cos sobie chce zrobic. Chciala do mnie przyjechac. Strasznie
+			spanikowalem wtedy - trzesly mi sie rece, mowilem jej ze zle wygladam zeby nie zaprzatala
+			sobie mna glowy - ale w glebi duszy tak naprawde chcialem aby przyjechala. Dalem jej adres
+			(mieszkalem wtedy w Krakowie jeszcze). Gdy do mnie przyjechala zapytala czy nie jestem glodny.
+			Poczulem sie bezpiecznie wtedy strasznie mimo ze malo sie oddzywalem. Byla godzina kolo 1 w
+			nocy wiec zapronowala maka. Caly czas do mnie rozmawiala. Chciala mi pomoc. Mowila ze pomoze
+			mi zalawtic specialiste. Bylem strasznie roztrzesiony, zaczela mnie bolec glowa. Gdy
+			siedzielismy w ciszy (czego bardzo sie wstydzilem) to powiedziala <b
+				>"Sory klimson niestety jutro mam mature nie chce cie zostawiac w takim stanie ale musze
+				wstac na mature"</b
+			>. Gdy mnie podwiozla do domu napisala pol godziny pozniej jakos ze nie wie czy to co
+			pwoiedziala ma sens ale ze bedzie dobrze. Pozniej przy okazji innych spotkan okazlo sie ze ma
+			bordera. Zaczalem ja zbyt obciazac moimi problemami ktorych nie mogla udzwignac przez swoje
+			problemy psychiczne wczesniej wspomnianego bordera. Pamietam ze wtedy wracalem tramwajem do
+			domu od dentysty. Strasznie sie wtedy na nia zdenerwowalem, napisalem jej ze czemu mi nie
+			odpisujesz i ze wiesz ze mi ciezko. Napisala ze nie moze tego ucignac ze zdanie "Nie moge bez
+			ciebie zyc" strasznie ja obciaza. Zerwala ze mna kontakt. Pamietam jak sie wtedy zalamalem,
+			jakby mnie psychiczne mlotem zakurwil. Chwile po tej sytuacji totalnie bez zapowiedzi moj
+			nastroj polecial w gore - co nie ma sensu kompletnie. Moj nastroj zaczal zbyt szybko sie
+			polepszac. Zaczalem zachowywac sie doslownie jak jakis maniak. Jakies monologi na strone
+			<a href="/lore">/lore</a>. Bylem wtedy przekonany ze zwalczylem zaburzenia osobowosci.
+			Dzialalem wtedy bardzo impulsywnie. Poklocilem sie z rodzicami, powiedzialem ze to ich wina ze
+			tak sie czuje i zerwalem kontakt. Zwyzywalem koordynatora w pracy i kolege. Bylem wtedy
+			nadmiernie pewny siebie. Gdy kierownik zaproponowal mi posade na informatyka ze zapyta sie
+			kogos czy nie ma miejsca - w mojej glowie pojawilo sie cos w stylu
+			<b>"Chce przekonwac tego informatyka do tego jaki jestem zajebisty"</b>. Bylem w takim amoku
+			ze chcialem wrecz zniszczyc zycie temu koordynarowi (mimo ze teraz sadze ze zle zrobil) to
+			wtedy bylem tak impulsywnie ze chcialem po prostu mu zniszczyc zycie. Zalilem sie do mojej
+			koordynatorki o moich prywatnych problemach, liderce oraz koledze ktoremu nidgy czego takiego
+			nie mowilem bo sie wstydzilem. Bylem nadmiernie pewny siebie. Na studiach doslownie
+			zachowywalem sie jak ekstrewertyk (mimo ze w depresyjnym stanie jest zupelnie odwrotnosc).
+			Szczerze mysle ze jak duzo pamietam - tak bardzo malo z tego okresu moge powiedziec bo
+			strasznie duzo impulsywnosci bylo wtedy, chaosu. Nie wspomne o tym ze jak juz mialem ten
+			podwyzszony nastroj dostalem leki od psychiatry - Asentre (leki na depresje i leki) - bo
+			czulem dziwny stres w srodku caly czas nie zwiazny z ludzmi. Pamietam ze mimo tego ze czulem
+			sie zle po nich (zawroty glowy, malo spalem, bylo mi goraco) to i tak cos mnie napedzalo dalej
+			do przodu.
+		</Paragraph>
+	</div>
+{/snippet}
+
+{#snippet MidSchool()}
+	<div class="flex flex-col gap-5">
+		<Heading id="midschool">Srodowisko w ktorym umarlem</Heading>
+		<Paragraph
+			>Chcialbym rowniez poruszyc temat gimnazjum. Pamietam ze same wejscie w ten okres byl dla mnie
+			ciezki. Mimo ze zapisalem sie z kolega do tej samej klasy to czulem sie dziwnie mimo ze nic
+			sie nie dzialo. Pewnego dnia moj kolega sie odwrocil ode mnie. Zaczalem byc przesladowany
+			przez ludzi z mojej klasy. Mimo ze wiem ze nie bylem tez bez winy to wiem ze bardzo mocno
+			bylem wtedy wysmiewany. Stalem sie posmiewiskiem nie tylko klasy - ale rowniez calej szkoly.
+			Czulem sei wtedy jak podczlowiek, ktory jest gorszy. Czesto jak szedlem do szkoly to balem sie
+			czy nie bede pobity przez kolegow z klasy. Dochodzilo do takich momentow ze balem sie isc do
+			szkoly. Pamietam ze byl taki dzien ze juz tak na tyle sie balem ze zrobie mi krzywde ze
+			poszedlem do psycholog. Wypuscila mnie wczesniej. Skrajnie sie balem kazdego dnia. Chcialem
+			zeby mnie akceptowali mimo wszystko. Czulem sie mega samotny z tym wszystkim. Rodzice mimo ze
+			probowali pomagac z policja to finalnie nie pomogli a nawet pokusil bym sie o stwierdzenie ze
+			pogorszyli sprawe. Pamietam ze mialem wtedy moment ze chce skonczyc ze soba. Bylem wyzywany od
+			nazwiska pedofila mordercy. Czulem sie wtedy okropnie samotny w klasie. Pamietam ze kolezanka
+			na przyrodzie nie miala gdzie usciasc a bylo miejsce kolo mnie. Byla strasznie niezadowolona
+			ze bedzie siedziec kolo mnie. Poczulem sie wtedy jak najwiekszy smiec jaki moze isteniec.
+			Bardzo sie ciesze ze juz ten przykry etap mam juz za soba.</Paragraph
+		>
+	</div>
+{/snippet}
+
+{#snippet Summary()}
+	<div class="flex flex-col gap-5">
+		<Heading id="summary">Podsumowanie</Heading>
+		<Paragraph
+			>Mimo ze z ta kolezanka nie mam kontaktu to wiem ze bardzo mi pomogla wejsc w ta moja osobista
+			manie i ten stabilny nastroj co pokazalo mi ze to co wczesniej czulem nie bylo normalne. Te
+			problemy z koncetracja, skrajny lek do ludzi. To ze wstydzilem sie siebie. Programowania - cos
+			co dla wielu jest czyms wow, dla mnie bylo powodem do wstydu. Jest mi bardzo przykro ze bylem
+			dla siebie tak okrutny. Przez wiele lat czulem jakbym nie pasowal do tego swiata. Trauma
+			gimnazjalna zablokowala we mnie moja normalna osobowosc. Nie wiem co to jest. Zauwazylem ze
+			nie musze przypisywac sobie latki zaburzen osobowosci itp aby potraktowac tego typu hustawki
+			nastrojów powaznie.
+		</Paragraph>
+	</div>
+{/snippet}
