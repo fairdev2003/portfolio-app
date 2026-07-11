efae36e1298b1538fd49953a64b16750eea580b8
fairdev2003 <jakubklimkiewicz70@gmail.com>
2026-07-11 21:52:13 +0200

    Added special redis loading

--- src/routes/objawy/+page.svelte
+++ src/routes/objawy/+page.svelte
@@ -1,5 +1,3 @@
 <script lang="ts">
 	import MarkdownRenderer from '../../components/blog/MarkdownRenderer.svelte';
 </script>
-
-<MarkdownRenderer markdown="https://api.klimson.dev/interface/bucket/klimson.dev/objawy.md" />
